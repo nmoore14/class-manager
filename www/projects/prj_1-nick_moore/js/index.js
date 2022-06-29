e974465febf1e9df5f47f1751551fd0a3aa1d00e
Nick Moore <nmoore@foreup.com>
2022-06-28 23:10:47 -0400

    Adding main section and styling

--- www/projects/prj_1-nick_moore/js/index.js
+++ www/projects/prj_1-nick_moore/js/index.js
@@ -9,6 +9,9 @@ const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme
 const headerImageLight = 'media/images/bg-wave-1.png';
 const headerImageDark = 'media/images/bg-wave-2.png';
 
+const gradientColor1 = document.getElementById('gradient-color-1');
+const gradientColor2 = document.getElementById('gradient-color-2');
+
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
@@ -29,6 +32,18 @@ function setHeaderImage() {
     } 
 }
 
+function updateBlobColors() {
+    let theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (theme === 'dark') {
+        gradientColor1.setAttribute('stop-color', '#34D399');
+        gradientColor2.setAttribute('stop-color', '#6D28D9');
+    } else {
+        gradientColor1.setAttribute('stop-color', '#EF5DA8');
+        gradientColor2.setAttribute('stop-color', '#C4B5FD');
+    }
+}
+
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
@@ -40,6 +55,7 @@ function switchTheme(e) {
     }   
 
     setHeaderImage();
+    updateBlobColors();
 }
 
 // General method calls
@@ -48,4 +64,5 @@ toggleSwitch.addEventListener('change', switchTheme, false);
 // Methods to run on window load
 window.onload = function() {
     setHeaderImage();
+    updateBlobColors();
 }
