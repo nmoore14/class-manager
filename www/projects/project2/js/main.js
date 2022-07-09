0956c9d76f795c0fe64800c9ca429836de855f8d
Nick Moore <nmoore@foreup.com>
2022-07-09 12:08:59 -0400

    Adding new conversion form

--- www/projects/project2/js/main.js
+++ www/projects/project2/js/main.js
@@ -31,21 +31,64 @@ toggleSwitch.addEventListener('change', switchTheme, false);
 * to celsius: (C x 9/5) + 32
 * to fahrenheit: (F -32) x 5/9
 */
-function calcTemp(ogTempType, temp) {
+var tempToConvertTo = 'celsius';
+
+function calcCels(temp) {
+    let newTemp = (temp - 32) * 5/9;
+    return Math.round(newTemp * 100) / 100;
+}
+
+function calcFah(temp) {
+    let newTemp = (temp * 9/5) + 32;
+    return Math.round(newTemp * 100) / 100;
+}
+
+function updateTemp(ogTempType, temp) {
     if (ogTempType === 'celsius') {
         // Set fahrenheit value
-        let newTemp = (temp - 32) * 5/9;
-        document.getElementById('fahrenheit-temp').value = Math.round(newTemp * 100) / 100;
+        document.getElementById('fahrenheit-temp').value = calcFah(temp);
     }
     if (ogTempType === 'fahrenheit') {
         // Set celsius value
-        let newTemp = (temp * 9/5) + 32;
-        document.getElementById('celsius-temp').value = Math.round(newTemp * 100) / 100; 
+        document.getElementById('celsius-temp').value = calcCels(temp); 
     }
 }
 
-document.querySelectorAll('.temp-input').forEach(tempInput => {
+function updateFormConversionHeader(ogTempType) {
+    if (ogTempType === 'celsius') {
+        tempToConvertTo = 'fahrenheit';
+    } else {
+        tempToConvertTo = 'celsius';
+    }
+}
+
+function calcTemp(e) {
+    e.preventDefault();
+    const ogTemp = document.getElementById('form-temp').value;
+    let newTemp = 0;
+
+    if (tempToConvertTo === 'fahrenheit') {
+       newTemp = calcFah(ogTemp);
+    }
+
+    if (tempToConvertTo === 'celsius') {
+        newTemp = calcCels(ogTemp);
+    }
+
+    document.getElementById('conversion').innerText = newTemp;
+    document.getElementById('conversion-subtitle').innerText = tempToConvertTo;
+}
+
+// Form conversion setup
+document.querySelectorAll('.form-radio').forEach(radio => {
+    radio.addEventListener('click', (e) => {
+        updateFormConversionHeader(e.target.value);
+    })
+})
+
+// Auto conversino section setup
+document.querySelectorAll('.auto-input').forEach(tempInput => {
     tempInput.addEventListener('change', (e) => {
-        calcTemp(e.target.name, e.target.value);
+        updateTemp(e.target.name, e.target.value);
     })
 })
