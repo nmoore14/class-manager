c0dd6ef6c12671542fa72120ed1598205a331980
Nick Moore <nmoore@foreup.com>
2022-07-09 23:19:10 -0400

    Styling and adding some more css flair

--- www/projects/project2/js/main.js
+++ www/projects/project2/js/main.js
@@ -66,17 +66,27 @@ function calcTemp(e) {
     e.preventDefault();
     const ogTemp = document.getElementById('form-temp').value;
     let newTemp = 0;
+    let tempRating = 'cold';
 
     if (tempToConvertTo === 'fahrenheit') {
-       newTemp = calcFah(ogTemp);
+        newTemp = calcFah(ogTemp);
+        if (newTemp > 32) {
+            tempRating = 'hot';
+        }
     }
 
     if (tempToConvertTo === 'celsius') {
         newTemp = calcCels(ogTemp);
+        if (newTemp > 0) {
+            tempRating = 'hot';
+        }
     }
 
-    document.getElementById('conversion').innerText = newTemp;
+    newTemp = `${newTemp}&deg;`;
+
+    document.getElementById('conversion').innerHTML = newTemp;
     document.getElementById('conversion-subtitle').innerText = tempToConvertTo;
+    changeGradient(tempRating);
 }
 
 // Form conversion setup
@@ -92,3 +102,22 @@ document.querySelectorAll('.auto-input').forEach(tempInput => {
         updateTemp(e.target.name, e.target.value);
     })
 })
+
+/*
+* DOM sytle manipulations
+*/
+const nestGradientStart = document.getElementById('nest-gradient-start');
+const nestGradientEnd = document.getElementById('nest-gradient-end');
+
+function changeGradient(tempRating) {
+    let tempStartColor = '#326BBB';
+    let tempEndColor = '#0047AB';
+
+    if (tempRating === 'hot') {
+        tempStartColor = '#DC143C';
+        tempEndColor = '#9A0E2A';
+    }
+
+    nestGradientStart.setAttribute('stop-color', tempStartColor);
+    nestGradientEnd.setAttribute('stop-color', tempEndColor);
+}
