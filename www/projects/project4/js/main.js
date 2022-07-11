da5c38f52e90eed53507e020b74b732b356f1dd1
Nick Moore <nmoore@foreup.com>
2022-07-11 17:07:16 -0400

    Finishing the logic for converting the data

---
+++ www/projects/project4/js/main.js
@@ -0,0 +1,19 @@
+const data = document.getElementById('data-input');
+const dataOutput = document.getElementById('data-output');
+
+function translateData() {
+    let newData = [];
+    let ogData = data.value;
+    splitData = ogData.split(';');
+
+    for (let i = 0; i < splitData.length; i++) {
+        if (splitData[i].includes(',')) {
+            newData.push(`"${splitData[i]}"`);
+            continue;
+        }
+
+        newData.push(splitData[i]);
+    }
+
+    dataOutput.innerText = newData.join(',');
+}
