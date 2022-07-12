6b4fda88b20ec7c0f1d96cc86bec2651f0a88ae3
Nick Moore <nmoore@foreup.com>
2022-07-12 18:43:40 -0400

    moving to array.map function to buid the new data.

--- www/projects/project4/js/main.js
+++ www/projects/project4/js/main.js
@@ -1,19 +1,18 @@
 const data = document.getElementById('data-input');
 const dataOutput = document.getElementById('data-output');
 
+function validateData(item) {
+    if (item.includes(',')) {
+        return `"${item}"`;
+    } 
+
+    return item;
+}
 function convertData() {
-    let newData = [];
     let ogData = data.value;
     splitData = ogData.split(';');
 
-    for (let i = 0; i < splitData.length; i++) {
-        if (splitData[i].includes(',')) {
-            newData.push(`"${splitData[i]}"`);
-            continue;
-        }
-
-        newData.push(splitData[i]);
-    }
+    let newData = splitData.map(validateData);
 
     dataOutput.innerText = newData.join(',');
 }
