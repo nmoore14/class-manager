fe365d32e5ef9cc168c6cecb506d7256ba14e095
Nick Moore <nmoore@foreup.com>
2022-07-23 19:20:07 -0400

    Adding keypress events and styling new modal

--- www/projects/project6/js/main.js
+++ www/projects/project6/js/main.js
@@ -4,31 +4,83 @@ $( document ).ready(() => {
     const modalBox = $('.modal-box');
     const closeModal = $('.close');
     const doors = $('.door');
+    const modalBtns = $('.btn');
 
     // Game variables
-    const key = 'Project6';
     var doorPrize;
     var prevDoor;
 
+    function getRandomPrizeAmount() {
+        return Math.floor(Math.random() * 100000);
+    }
+
     function getDoorPrize(doorValue) {
-        const newDoorPrize = Math.floor(Math.random() * 100000);
+        const newDoorPrize = getRandomPrizeAmount();
 
         if (! doorPrize || prevDoor != doorValue) {
             doorPrize = newDoorPrize;
             prevDoor = doorValue;
         }
     }
+
+    function setModalFooter(doorValue) {
+        console.log('Set Modal Footer called');
+        $('#modal-footer').html(`<p class="guide-text">You have selected door ${doorValue}</p>`);
+    }
+
+    function setTakeAmount() {
+        const takeAmount = getRandomPrizeAmount();
+        $('#take-btn').attr("data-take", takeAmount);
+        $('#take-btn').html(`Take ${takeAmount}`);
+    }
+
+    function confirmSelection(btnValue) {
+        if (confirm("Are you sure?")) {
+            console.log(doorPrize);
+            hideModal();
+            modalBox.toggleClass("modal-box-visible");
+            doorPrize = '';
+            prevDoor = '';
+        } else {
+            console.log('Going back');
+        }
+    }
+
+    function hideModal() {
+        modal.css("display", "none");
+
+    }
     
     // Door event handling
     $.each(doors, (_, door) => {
         $(door).click((e) => {
             const doorValue = $(e.currentTarget).data("value");
+            $(e.currentTarget).addClass('door-disabled');
             getDoorPrize(doorValue);
+            setTakeAmount();
             modal.css("display", "flex");
             setTimeout(() => {
                 modalBox.toggleClass("modal-box-visible")
+                setModalFooter(doorValue);
             }, 250)
         });
+    })
+
+    $(window).keypress((e) => {
+        switch(e.which) {
+            case 49:
+                $('.js-door-1').click();
+                break;
+            case 50:
+                $('.js-door-2').click();
+                break;
+            case 51:
+                $('.js-door-3').click();
+                break;
+            default:
+                break;
+        }
+
     })
 
     // Modal event handling
@@ -42,8 +94,15 @@ $( document ).ready(() => {
     })
 
     $(closeModal).click((e) => {
-        modal.css("display", "none");
+        hideModal();
         modalBox.toggleClass("modal-box-visible");
+    })
+
+    $.each(modalBtns, (_, btn) => {
+        $(btn).click((e) => {
+            const btnValue = $(e.currentTarget).data("value");
+            confirmSelection(btnValue);
+        });
     })
 
 });
